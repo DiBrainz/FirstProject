6684a5619370afbd994f571d05fcd7b4dc012b65
manukovsky.da <manukovsky.da@DESKTOP-5DNQV0G>
2022-04-05 12:04:34 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write($"Введите минимальную температуру за сутки>");
+
+string min = Console.ReadLine();
+int minO = Convert.ToInt32(min);
+
+Console.Write($"Введите максимальную температуру за сутки>");
+
+string max = Console.ReadLine();
+int maxO = Convert.ToInt32(max);
+
+double middle = (double)(minO + maxO)/2;
+Console.WriteLine($"Средняя суточная темература составляет {middle}°");
